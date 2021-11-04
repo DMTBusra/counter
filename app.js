cd8b776769467bf39ef3229af8f1973fb5097449
DMTBusra <gencbusra4325@gmail.com>
2021-11-04 13:51:41 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+let number = document.getElementsByClassName("number")[0];
+let decrease_btn = document.getElementsByClassName("Decrease")[0];
+let reset_btn = document.getElementsByClassName("Reset")[0];
+let increase_btn = document.getElementsByClassName("Increase")[0];
+let container = document.getElementsByClassName("container")[0];
+
+let count = 0;
+container.addEventListener("click",(e)=>{
+    
+    if(e.target.className =="decrease"){
+        count += 1;
+        color(count);      
+    }
+    else if(e.target.className =="increase"){
+        count -= 1;
+        color(count);     
+    }
+    else if(e.target.className =="reset"){
+        count = 0;   
+        color(count);
+    }
+});
+
+
+function color (count){
+    if(count == 0){
+        number.style.color = "purple";
+}
+    else if (count <0){
+        number.style.color = "green";
+}
+    else{ 
+        number.style.color = "red";
+    }
+    number.innerText = count;
+};
+ 
